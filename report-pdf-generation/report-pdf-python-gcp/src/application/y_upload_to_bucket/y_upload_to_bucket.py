1269623015b575aa5b5185c4931513f873976014
Sigma Solo <33127626+ditlef9@users.noreply.github.com>
2025-04-18 07:34:12 +0200

    Update y_upload_to_bucket.py

--- report-pdf-generation/report-pdf-python-gcp/src/application/y_upload_to_bucket/y_upload_to_bucket.py
+++ report-pdf-generation/report-pdf-python-gcp/src/application/y_upload_to_bucket/y_upload_to_bucket.py
@@ -5,7 +5,7 @@ from src.utils.google_bucket_write_from_filename import google_bucket_write_from
 
 def y_upload_to_bucket(bucket: Bucket, pdf_file_full_path: str, bucket_target_full_path: str):
 
-    print("b_create_tmp() · Uploading PDF to ")
+    print("main()·y_upload_to_bucket() · Uploading PDF to ")
     google_bucket_write_from_filename(bucket=bucket,
                                       file_to_upload_full_path=pdf_file_full_path,
                                       bucket_target_full_path=bucket_target_full_path)
