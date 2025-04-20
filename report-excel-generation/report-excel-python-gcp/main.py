30bf3200cf7b5722ed66cd63891411106892c16c
Sigma Solo <33127626+ditlef9@users.noreply.github.com>
2025-04-20 09:15:12 +0200

    Update main.py

--- report-excel-generation/report-excel-python-gcp/main.py
+++ report-excel-generation/report-excel-python-gcp/main.py
@@ -28,8 +28,8 @@ def main(request: flask.wrappers.Request):
         google_cloud_project_id = os.getenv('GOOGLE_CLOUD_PROJECT_ID')
         secret_str = google_secret_manager_access_secret_version(project_id=google_cloud_project_id, secret_id="report-excel-services")
     except Exception as e:
-        print(f"{log_headline} Could not access google secret e={e}")
-        raise Exception(f"main()·send_email() Could not access google secret e={e}")
+        print(f"{log_headline}· Could not access google secret e={e}")
+        raise Exception(f"{log_headline}· Could not access google secret e={e}")
     secret_list: list = json.loads(secret_str)
     gmail_sender_email: str = secret_list['gmail_sender_email']
     gmail_app_password: str = secret_list['gmail_app_password']
